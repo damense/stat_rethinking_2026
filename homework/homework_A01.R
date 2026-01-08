c32cb30ef0e97ee154f7c5d05d7533a5816acc7a
David M. <d.mendsev@gmail.com>
2026-01-08 23:05:23 +0100

    2m7

--- homework/homework_A01.R
+++ homework/homework_A01.R
@@ -45,10 +45,98 @@ print(paste("the solution is",0.3*0.5/(0.3*0.5+1*0.5)))
 ### side 2: white
 ## If we pull a card and it's black, probability of other side being black is 2/3
 # ---- 2M5 ----
+## If we pull card A
+### side 1: black -> Other side: black
+### side 2: black -> Other side: black
+## If we pull card D
+### side 1: black -> Other side: black
+### side 2: black -> Other side: black
+## If we pull card B
+### side 1: black -> Other side: white
+### side 2: white
+## If we pull card C
+### side 1: white
+### side 2: white
+## If we pull a card and it's black, probability of other side being black is 4/5
 # ---- 2M6 ----
+## If we pull card A
+### side 1: black -> Other side: black
+### side 2: black -> Other side: black
+## If we pull card B
+### side 1: black -> Other side: white
+### side 2: white
+## If we pull card B
+### side 1: black -> Other side: white
+### side 2: white
+## If we pull card C
+### side 1: white
+### side 2: white
+## If we pull card C
+### side 1: white
+### side 2: white
+## If we pull card C
+### side 1: white
+### side 2: white
+## If we pull a card and it's black, probability of other side being black is 2/4
 # ---- 2M7 ----
+## I did it in the whiteboard but there are 8 possibilities of first card with a black side and second card with a white side. 6 of them have black on the side of the other card 3/4=0.75
 # ---- 2H1 ----
 # ---- 2H2 ----
 # ---- 2H3 ----
 # ---- 2H4 ----
 # ---- A1 ----
+library(tidyverse)
+library(gtools)
+n_people <- 10
+values <- c(T,F)
+res <-as.data.frame(
+        permutations(n=length(values),
+                     r=n_people,
+                     v=values,
+                     repeats.allowed = T)
+)
+# Using the “garden of forking data” approach, how many ways are there to realize
+# this sample (8 out of 10), if all participants are honest?
+res %>%
+        mutate(sum = as.factor(rowSums(., na.rm=TRUE))) %>% 
+        group_by(sum) %>% 
+        summarise(n=n()) %>% 
+        mutate(freq=n/sum(n))
+
+### There are 45 out of 1024 ways = 0.0439 to realize (8 out of 10)
+
+
+# How many ways, if 5 of the participants are honest and the other 3 lie if the have a False?
+###If 5 participants are honest and the other five say they got "heads"
+
+
+v_cols <- paste0("V", seq_len(5))
+
+res %>%
+        mutate(across(all_of(v_cols), ~ TRUE)) %>%
+        mutate(sum = rowSums(across(starts_with("V")), na.rm = TRUE)) %>%
+        count(sum, name = "n") %>%
+        mutate(freq = n / sum(n)) %>%
+        filter(sum == 8) %>%
+        pull(n) %>%
+        { if (length(.) == 0) 0 else . }
+
+### The amount is 320/1024=0.3125
+# Can you figure out the number of honest participants that maximizes the number of ways to realize the observed sample
+#(8 out of 10)
+map_dbl(
+        seq_len(10),
+        function(k) {
+                v_cols <- paste0("V", seq_len(k))
+                
+                res %>%
+                        mutate(across(all_of(v_cols), ~ TRUE)) %>%
+                        mutate(sum = rowSums(across(starts_with("V")), na.rm = TRUE)) %>%
+                        count(sum, name = "n") %>%
+                        mutate(freq = n / sum(n)) %>%
+                        filter(sum == 8) %>%
+                        pull(n) %>%
+                        { if (length(.) == 0) 0 else . }
+        }
+)
+### The amount is 6
